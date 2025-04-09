6ecd26841dfaa76a83caf7f5c7ab7b5169c7c733
phquartin <phquartinmatos@sempreceub.com>
2025-04-09 17:04:33 -0300

    refactor: simplifica métodos em ClientService para uma linha
    Unifica métodos no ClientService em uma única linha para maior concisão. Essa alteração melhora a legibilidade do código sem alterar a funcionalidade existente.

--- src/main/java/com/agencia/project/service/ClientService.java
+++ src/main/java/com/agencia/project/service/ClientService.java
@@ -7,17 +7,14 @@ import java.util.Optional;
 
 public final class ClientService {
 
-    public static void save(ClientModel client) {
-        ClientDAO.createClient(client);
-    }
+    public static void save(ClientModel client) { ClientDAO.createClient(client); }
+
     public static ClientModel findById(int id) {
         Optional<ClientModel> clientModel = ClientDAO.readClientById(id);
         return clientModel.orElseThrow(() -> new NullPointerException("Cliente nao encontrado"));
     }
-    public static void update(ClientModel client) {
-        ClientDAO.updateClient(client);
-    }
-    public static void delete(int id) {
-        ClientDAO.deleteClient(id);
-    }
+
+    public static void update(ClientModel client) { ClientDAO.updateClient(client); }
+
+    public static void delete(int id) { ClientDAO.deleteClient(id); }
 }
