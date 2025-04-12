01b7947ee4c519ba0d0868321a867383f2154bea
phquartin <phquartinmatos@sempreceub.com>
2025-04-12 18:18:25 -0300

    refactor(client): reformular modelo de cliente usando Lombok
    - Removido código boilerplate
    - Adicionado anotação @Data do Lombok para gerar getters, setters e outros métodos automaticamente
    - Modificado o tipo de id de Integer para Long
    - Removido sobrecarga de construtores
    - Simplificado a estrutura da classe

--- src/main/java/com/agencia/project/model/client/ClientModel.java
+++ src/main/java/com/agencia/project/model/client/ClientModel.java
@@ -1,78 +1,15 @@
 package com.agencia.project.model.client;
 
-import java.util.Objects;
+import lombok.Data;
 
+@Data
 public class ClientModel {
 
-    private Integer id;
-    private final String name;
-    private final int age;
-    private final String email;
-    private final String document;
-    private final Nationality nationality;
+    private final Long id;
+    private String name;
+    private int age;
+    private String email;
+    private String document;
+    private Nationality nationality;
 
-    public ClientModel(Integer id, String name, int age, String email, String document, Nationality nationality) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.document = document;
-        this.nationality = nationality;
-    }
-
-    public ClientModel(String name, int age, String email, String document, Nationality nationality) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-        this.document = document;
-        this.nationality = nationality;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        ClientModel that = (ClientModel) o;
-        return Objects.equals(id, that.id) && Objects.equals(document, that.document);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, document);
-    }
-
-    @Override
-    public String toString() {
-        return "ClientModel{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                ", document='" + document + '\'' +
-                ", nationality=" + nationality +
-                '}';
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getDocument() {
-        return document;
-    }
-
-    public Nationality getNationality() {
-        return nationality;
-    }
 }
